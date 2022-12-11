693921ac53513021b98fd715e4a799ab33df8839
ClementGuichet <Clement.Guichet@univ-grenoble-alpes.fr>
2022-12-11 12:58:37 +0100

    Added Largest Connected Component for threshold analysis choice

--- _03_Global_Metrics.R
+++ _03_Global_Metrics.R
@@ -10,60 +10,44 @@ library(jsonlite)
 
 
 rm(list = ls())
-options(max.print = 99999)
-
 # Import processed data---------------------------------------------------------
 source("_01_DataManip.R")
 
 ################################################################################
-# Get mean r per threshold per subject with absolute threshold
-# Mean_r <- as.data.frame((fromJSON("Mean_r.json"))) %>%
-#   plyr::rename(c("V1" = "0.05")) %>%
-#   plyr::rename(c("V2" = "0.07")) %>%
-#   plyr::rename(c("V3" = "0.1")) %>%
-#   plyr::rename(c("V4" = "0.15")) %>%
-#   plyr::rename(c("V5" = "0.2")) %>%
-#   plyr::rename(c("V6" = "0.25")) %>%
-#   pivot_longer(
-#     cols = c("0.05", "0.07", "0.1", "0.15", "0.2", "0.25"),
-#     names_to = "threshold",
-#     values_to = "Mean_r"
-#   ) %>%
-#   mutate(Subj_ID = rep(seq_len(72), each = 6))
-#
-# data_per_subject_combined_bu <- merge(data_per_subject_combined_bu, Mean_r, by = c("Subj_ID", "threshold")) %>%
-#   arrange(Subj_ID, threshold)
-#
-# test_mean_r <- data_per_subject_combined_bu %>% subset(threshold == "0.2")
-# cor.test(test_mean_r$Mean_r, test_mean_r$Age)
-# # Get proportion of LLC per threshold
-# # Connected components generate with consensus based approach, niter = 1000, tau = .5
-# LLC <- as.data.frame((fromJSON("consensus_LLC.json"))) %>%
-#   pivot_longer(
-#     cols = c("V1", "V2", "V3", "V4", "V5", "V6"),
-#     names_to = "threshold"
-#   ) %>%
-#   count(threshold, value) %>%
-#   group_by(threshold) %>%
-#   mutate(prop = prop.table(n))
-#
-# threshold <- c(.05, .07, .1, .15, .2, .25)
-# LLC_coeff <- c(0.442, 0.557, 1, 1, 1, 1) # Input the largest connected component
-# LLC_plot <- cbind(threshold, LLC_coeff)
-# data_per_subject_combined_bu <- merge(data_per_subject_combined_bu, LLC_plot, by = "threshold") %>% arrange(Subj_ID, threshold)
+listfile_components <- list.files(getwd(), pattern = "*.txt")[grep("components", list.files(getwd(), pattern = "*.txt"))]
+n_subj <- 72
+n_threshold <- 8
+
+components <- ldply(listfile_components, read.table, header = T, sep = "\t") %>%
+  mutate(threshold = rep(c(.05, .07, .1, .12, .15, .17, .2, .25), each = n_subj)) %>%
+  relocate(threshold, .after = (X)) %>%
+  plyr::rename(c("X" = "Subj_ID")) %>%
+  replace("Subj_ID", rep(seq_len(n_subj), times = n_threshold)) %>%
+  pivot_longer(
+    cols = !c("Subj_ID", "threshold"),
+    names_to = "Region",
+    values_to = "components"
+  )
+
+# # Get proportion of LLC per threshold across subjects
+LLC <- components %>% group_by(Subj_ID) %>% count(threshold, components) %>% 
+  group_by(Subj_ID, threshold) %>% mutate(prop = prop.table(n)) %>% 
+  slice_max(prop, n = 1) %>% 
+  group_by(threshold) %>% 
+  summarise_at(vars(prop), mean) %>% 
+  plyr::rename(c("prop" = "Largest Connected Component"))
+
+data_full_per_subject_LLC <- merge(data_full_per_subject, LLC, by = "threshold") %>% 
+  arrange(Subj_ID, threshold)
 
 # Evolution of Global metrics - what is the optimal threshold?
-evo <- data_full_per_subject %>%
+evo <- data_full_per_subject_LLC %>%
   group_by(threshold) %>%
-  summarise_at(vars(Eglob, Clustering_coeff_glob, Eloc, Q_consensus), funs(mean)) %>%
+  summarise_at(vars(Eglob, Clustering_coeff_glob, Eloc, Q_consensus, `Largest Connected Component`), funs(mean)) %>%
   pivot_longer(
     cols = !c("threshold"),
     names_to = "Metrics"
   )
-
-
-# Choosing threshold = 20% for subsequent analyses
-# Number of communities is 3 throughout the thresholds
 
 plotly::ggplotly(
   evo %>%
@@ -76,7 +60,7 @@ plotly::ggplotly(
     ylab("") +
     scale_x_continuous(breaks = c(0.05, 0.07, 0.1, 0.12, 0.15, 0.17, 0.2, 0.25)) +
     scale_y_continuous(breaks = seq(0, 1, 0.05)) +
-    coord_cartesian(ylim = c(0.2, 0.8)) +
+    coord_cartesian(ylim = c(0.2, 1)) +
     ggpubr::theme_pubclean()
 )
 
