7b706e9ccda0473e2bd0bb1e7f83c6f061d3d4e4
ClementGuichet <Clement.Guichet@univ-grenoble-alpes.fr>
2022-12-28 17:29:03 +0100

    Using Cruz modified extension for geomMean Updated functions & bootstrap

--- _08_LogRatios&Bootstrap.R
+++ _08_LogRatios&Bootstrap.R
@@ -1,16 +1,20 @@
-
 ################################################################################
-# LOG-RATIO of the Topologico-functional profile ------------------------------
+# LOG-RATIO of the Topologico-functional profile at the RSN level --------------
 ################################################################################
 source("_07_Entropy.R")
+source("_geometricmeanCruz.R")
 
-# Difference in the proportion of each functional role within each RSN for the two selected clusters
-
-interaction_Age_FuncRole_RSN <- function(cluster1, cluster2, max, min, max2, min2, alpha, divergent_RSN_modular, divergent_RSN_interareal) {
+# Ratio of the proportion of each functional role within each RSN between the two selected clusters
+interaction_Age_FuncRole_RSN <- function(cluster1, cluster2, max, min, max2, min2, alpha, RSN_modular, RSN_interareal) {
   
   data_cluster_selection(cluster1, cluster2)
   
-  delta_proportion_a1 <- tmp_cluster_final %>%
+  ##############################################################################
+  # For modular roles
+  ##############################################################################
+  
+  # Proportion of each functional role within each RSN for each subject
+  delta_proportion_a <<- tmp_cluster_final %>%
     group_by(`1st_network`, Region, Subj_ID, Age_group, Hub_consensus) %>%
     summarise(n = n()) %>%
     mutate(freq = n / sum(n)) %>%
@@ -20,38 +24,31 @@ interaction_Age_FuncRole_RSN <- function(cluster1, cluster2, max, min, max2, min
     group_by(`1st_network`, Subj_ID, Age_group) %>%
     summarize_at(vars(Connector:Satellite), mean) %>%
     ungroup() %>% 
-    pivot_longer(cols = !c("1st_network", "Subj_ID", "Age_group"), names_to = "Hub_consensus", values_to = "freq")
-  
-  count_zero <- delta_proportion_a1 %>% 
-    group_by(`1st_network`, Hub_consensus) %>% count(Age_group, freq) %>% 
-    group_by(`1st_network`, Age_group, Hub_consensus) %>% 
-    mutate(n = prop.table(n)) %>% 
-    filter(freq != 0) %>% 
-    group_by(`1st_network`, Age_group, Hub_consensus) %>% 
-    summarize_at(vars(n), sum)
-  
-  
-  delta_proportion_a <- delta_proportion_a1 %>% 
-    filter(freq != 0) %>% 
-    # Compute the geometric mean for each RSN
+    pivot_longer(cols = !c("1st_network", "Subj_ID", "Age_group"), names_to = "Hub_consensus", values_to = "freq") %>% 
     group_by(`1st_network`, Age_group, Hub_consensus) %>%
-    summarise_at(vars(freq), funs(geometricmean(.))) %>% 
-    merge(., count_zero, by = c("1st_network", "Age_group", "Hub_consensus")) %>% 
-    mutate(bi_geometric_mean = freq*n) %>% 
+    summarise_at(vars(freq), funs(geomMeanExtension(., epsilon = 1e-5))) %>% 
     group_by(`1st_network`, Hub_consensus) %>%
-    mutate(delta_freq = log(bi_geometric_mean / lead(bi_geometric_mean))) %>% 
+    # Compute the log ratios
+    mutate(delta_freq = log(freq / lead(freq))) %>% 
     dplyr::select(-c(freq, Age_group)) %>%
     na.omit()
   
-  
-  Radar_functional_role_RSN_delta_a <<-
-    delta_proportion_a %>%
-    dplyr::select(`1st_network`, Hub_consensus, delta_freq) %>%
-    filter(grepl(divergent_RSN_modular, `1st_network`)) %>%
-    spread(`1st_network`, delta_freq) %>%
-    remove_rownames() %>%
-    column_to_rownames(var = "Hub_consensus")
-  
+  if (RSN_modular == "All") {
+    Radar_functional_role_RSN_delta_a <<-
+      delta_proportion_a %>%
+      dplyr::select(`1st_network`, Hub_consensus, delta_freq) %>%
+      spread(`1st_network`, delta_freq) %>%
+      remove_rownames() %>%
+      column_to_rownames(var = "Hub_consensus")
+  } else {
+    Radar_functional_role_RSN_delta_a <<-
+      delta_proportion_a %>%
+      dplyr::select(`1st_network`, Hub_consensus, delta_freq) %>%
+      filter(grepl(RSN_modular, `1st_network`)) %>%
+      spread(`1st_network`, delta_freq) %>%
+      remove_rownames() %>%
+      column_to_rownames(var = "Hub_consensus")
+  }
   
   radarplotting_overlap(Radar_functional_role_RSN_delta_a, max, min, 1, 1,
                         alpha = alpha, label_size = 1,
@@ -68,7 +65,9 @@ interaction_Age_FuncRole_RSN <- function(cluster1, cluster2, max, min, max2, min
   
   
   ##############################################################################
+  # For interareal roles
   ##############################################################################
+  
   delta_proportion_b1 <- tmp_cluster_final %>%
     group_by(`1st_network`, Region, Subj_ID, Age_group, Bridgeness) %>%
     summarise(n = n()) %>%
@@ -79,36 +78,32 @@ interaction_Age_FuncRole_RSN <- function(cluster1, cluster2, max, min, max2, min
     group_by(`1st_network`, Subj_ID, Age_group) %>%
     summarize_at(vars(Global_Bridge:Super_Bridge), mean) %>%
     ungroup() %>% 
-    pivot_longer(cols = !c("1st_network", "Subj_ID", "Age_group"), names_to = "Bridgeness", values_to = "freq")
-  
-  count_zero <- delta_proportion_b1 %>% 
-    group_by(`1st_network`, Bridgeness) %>% count(Age_group, freq) %>% 
+    pivot_longer(cols = !c("1st_network", "Subj_ID", "Age_group"), names_to = "Bridgeness", values_to = "freq") %>% 
     group_by(`1st_network`, Age_group, Bridgeness) %>% 
-    mutate(n = prop.table(n)) %>% 
-    filter(freq != 0) %>% 
-    group_by(`1st_network`, Age_group, Bridgeness) %>% 
-    summarize_at(vars(n), sum)
-  
-  
-  delta_proportion_b <<- delta_proportion_b1 %>% 
-    filter(freq != 0) %>% 
-    # Compute the geometric mean for each RSN
-    group_by(`1st_network`, Age_group, Bridgeness) %>%
-    summarise_at(vars(freq), funs(geometricmean(.))) %>% 
-    merge(., count_zero, by = c("1st_network", "Age_group", "Bridgeness")) %>% 
-    mutate(bi_geometric_mean = freq*n) %>% 
+    summarise_at(vars(freq), funs(geomMeanExtension(., epsilon = 1e-5))) %>% 
     group_by(`1st_network`, Bridgeness) %>%
-    mutate(delta_freq = log(bi_geometric_mean / lead(bi_geometric_mean))) %>% 
+    # Compute the log ratios
+    mutate(delta_freq = log(freq / lead(freq))) %>% 
     dplyr::select(-c(freq, Age_group)) %>%
     na.omit()
   
-  Radar_functional_role_RSN_delta_b <<-
-    delta_proportion_b %>%
-    dplyr::select(`1st_network`, Bridgeness, delta_freq) %>%
-    filter(grepl(divergent_RSN_interareal, `1st_network`)) %>%
-    spread(`1st_network`, delta_freq) %>%
-    remove_rownames() %>%
-    column_to_rownames(var = "Bridgeness")
+  if (RSN_interareal == "All") {
+    Radar_functional_role_RSN_delta_b <<-
+      delta_proportion_b %>%
+      dplyr::select(`1st_network`, Bridgeness, delta_freq) %>%
+      spread(`1st_network`, delta_freq) %>%
+      remove_rownames() %>%
+      column_to_rownames(var = "Bridgeness")
+  } else {
+    Radar_functional_role_RSN_delta_b <<-
+      delta_proportion_b %>%
+      dplyr::select(`1st_network`, Bridgeness, delta_freq) %>%
+      filter(grepl(RSN_interareal, `1st_network`)) %>%
+      spread(`1st_network`, delta_freq) %>%
+      remove_rownames() %>%
+      column_to_rownames(var = "Bridgeness")
+  }
+  
   
   radarplotting_overlap(Radar_functional_role_RSN_delta_b, max2, min2, 1, 1,
                         alpha = alpha, label_size = 1,
@@ -122,10 +117,20 @@ interaction_Age_FuncRole_RSN <- function(cluster1, cluster2, max, min, max2, min
     text.col = "black", cex = 1, pt.cex = 2
   )
 }
+
+
+# Choose the clusters
+# Max and min value on radar plot grid
+# mfrow settings
+# alpha level
+# RSN to be displayed -- either "All" or specify the RSN with | for separation
+
 interaction_Age_FuncRole_RSN("Young", "Old", 1, -1, 1, -1, 0.1,
-                             divergent_RSN_modular = "Auditory|CON|DMN|FPN|Language|SMN",
-                             divergent_RSN_interareal = "Auditory|CON|DMN|FPN|Language|SMN"
+                             RSN_modular = "CON|DMN|FPN|Language|SMN",
+                             RSN_interareal = "Auditory|CON|DMN|FPN|Language|SMN"
 )
+
+
 
 
 ################################################################################
@@ -145,7 +150,7 @@ interaction_Age_FuncRole_RSN("Young", "Old", 1, -1, 1, -1, 0.1,
 # If the confidence interval contains the value ‘0’, no difference between the two groups for this particular cluster/RSN combination is concluded.
 # Only combinations for which the intervals are outside 0 are considered responsible for the cluster differences.
 
-bootstrap_ci <- function(n_boot, cluster1, cluster2, divergent_RSN_modular, divergent_RSN_interareal) {
+bootstrap_ci <- function(n_boot, cluster1, cluster2, RSN_modular, RSN_interareal) {
   
   data_cluster_selection(cluster1, cluster2)
   
@@ -155,13 +160,13 @@ bootstrap_ci <- function(n_boot, cluster1, cluster2, divergent_RSN_modular, dive
     mutate(freq = n / sum(n)) %>%
     spread(Hub_consensus, freq) %>%
     dplyr::select(-n) %>%
-    mutate_all(., ~ replace(., is.na(.), 0)) %>%
-    group_by(`1st_network`, Region, Age_group, Subj_ID) %>%
-    summarize_at(vars(Connector:Satellite), sum) %>%
-    ungroup() %>%
-    filter(grepl(divergent_RSN_modular, `1st_network`)) %>% 
-    # Draw samples with replacement for every region, RSN, and Age_group
-    group_by(Age_group, `1st_network`, Region, .add = TRUE) %>%
+    mutate_all(., ~ replace(., is.na(.), 0)) %>% 
+    group_by(`1st_network`, Subj_ID, Age_group) %>%
+    summarize_at(vars(Connector:Satellite), mean) %>%
+    ungroup() %>% 
+    filter(grepl(RSN_modular, `1st_network`)) %>% 
+    # Draw samples with replacement for every RSN, and Age_group
+    group_by(Age_group, `1st_network`, .add = TRUE) %>%
     group_split()
   
   list_boot <- list()
@@ -172,128 +177,98 @@ bootstrap_ci <- function(n_boot, cluster1, cluster2, divergent_RSN_modular, dive
     for (j in 1:length(tmp_bis)) {
       list_boot_bis[[j]] <- rbindlist(tmp_bis[j]) %>% mutate(n_data_boot = rep(j, times = length(nrow(.))))
     }
-    list_boot[[i]] <- rbindlist(list_boot_bis)
+    delta_boot_a <- rbindlist(list_boot_bis) %>% as.data.frame() %>%
+      pivot_longer(cols = !c("n_data_boot", "n_data_split", "1st_network", "Subj_ID", "Age_group"), names_to = "Hub_consensus", values_to = "freq") %>% 
+      group_by(`1st_network`, Age_group, Hub_consensus) %>%
+      summarise_at(vars(freq), funs(geomMeanExtension(., epsilon = 1e-5))) 
+ 
+    list_boot[[i]] <- delta_boot_a
   }
   
   # 1000 samples for each Age_group*1st_network*Region combination
-  # So that for each region for each RSN, and within each Age_group, 1000 resamples of the same size are taken
-  resamples <- rbindlist(list_boot)
+  # So that for each <- <- <- <-  region for each RSN, and within each Age_group, 1000 resamples of the same size are taken
   
-  delta_proportion_boot_a <- resamples %>%
-    group_by(n_data_boot, n_data_split, `1st_network`, Region, Age_group) %>%
-    summarize_at(vars(Connector:Satellite), mean) %>%
-    ungroup()
+  resamples <- rbindlist(list_boot) %>% as.data.frame()
   
-  # Multiplicative replacement at the regional and Age_group level
-  replacement_a <- delta_proportion_boot_a %>%
-    group_by(n_data_boot, .add = TRUE) %>%
-    group_split()
-  
-  list_replacement <- list()
-  for (i in 1:length(replacement_a)) {
-    list_replacement[[i]] <- multRepl(rbindlist(replacement_a[i]) %>%
-                                        dplyr::select(Connector:Satellite), dl = rep(1, 4), label = 0, frac = 1e-5)
-  }
-  
-  delta_a_detail <<- cbind(
-    delta_proportion_boot_a %>%
-      dplyr::select(n_data_boot, n_data_split, `1st_network`:Age_group),
-    rbindlist(list_replacement)
-  ) %>%
-    pivot_longer(
-      cols = !c("n_data_boot", "n_data_split", "1st_network", "Region", "Age_group"),
-      names_to = "Hub_consensus", values_to = "freq"
-    ) %>%
-    # Compute the difference in proportion of a given functional role within each RSN
-    group_by(n_data_boot, `1st_network`, Age_group, Hub_consensus) %>%
-    summarize_at(vars(freq), funs(geometricmean(.))) %>% 
+  log_ratio_boot_a <- resamples %>% 
     group_by(n_data_boot, `1st_network`, Hub_consensus) %>%
-    # Take the log ratios of geometric means
-    mutate(delta_freq = log(freq / lead(freq))) %>%
+    # Compute the log ratios
+    mutate(delta_freq = log(freq / lead(freq))) %>% 
     dplyr::select(-c(freq, Age_group)) %>%
     na.omit()
   
-  summary_bootstrap_a <<- delta_a_detail %>%
+  summary_bootstrap_a <<- log_ratio_boot_a %>%
     group_by(`1st_network`, Hub_consensus) %>%
     get_summary_stats(delta_freq, type = "full") %>%
     mutate(mean_025 = mean - ci) %>%
     mutate(mean_0975 = mean + ci) %>%
     dplyr::select(`1st_network`, Hub_consensus, mean, mean_025, mean_0975)
   
-  data_boot <- tmp_cluster_final %>%
+  ##############################################################################
+  ##############################################################################
+  
+  data_boot_bis <- tmp_cluster_final %>%
     group_by(`1st_network`, Region, Subj_ID, Age_group, Bridgeness) %>%
     summarise(n = n()) %>%
     mutate(freq = n / sum(n)) %>%
     spread(Bridgeness, freq) %>%
     dplyr::select(-n) %>%
-    # Make sure comparisons with missing functional roles can be achieved
-    mutate_all(., ~ replace(., is.na(.), 0)) %>%
-    ungroup() %>%
-    filter(grepl(divergent_RSN_interareal, `1st_network`)) %>% 
-    group_by(Age_group, `1st_network`, Region, .add = TRUE) %>%
+    mutate_all(., ~ replace(., is.na(.), 0)) %>% 
+    group_by(`1st_network`, Subj_ID, Age_group) %>%
+    summarize_at(vars(Global_Bridge:Super_Bridge), mean) %>%
+    ungroup() %>% 
+    filter(grepl(RSN_interareal, `1st_network`)) %>% 
+    # Draw samples with replacement for every RSN, and Age_group
+    group_by(Age_group, `1st_network`, .add = TRUE) %>%
     group_split()
   
   list_boot <- list()
-  for (i in 1:length(data_boot)) {
-    tmp <- rbindlist(data_boot[i]) %>% mutate(n_data_split = rep(i, times = length(nrow(.))))
+  for (i in 1:length(data_boot_bis)) {
+    tmp <- rbindlist(data_boot_bis[i]) %>% mutate(n_data_split = rep(i, times = length(nrow(.))))
     tmp_bis <- lapply(1:n_boot, function(i) tmp[sample(nrow(tmp), replace = TRUE), ])
     list_boot_bis <- list()
     for (j in 1:length(tmp_bis)) {
       list_boot_bis[[j]] <- rbindlist(tmp_bis[j]) %>% mutate(n_data_boot = rep(j, times = length(nrow(.))))
     }
-    list_boot[[i]] <- rbindlist(list_boot_bis)
+    delta_boot_b <- rbindlist(list_boot_bis) %>% as.data.frame() %>%
+      pivot_longer(cols = !c("n_data_boot", "n_data_split", "1st_network", "Subj_ID", "Age_group"), names_to = "Bridgeness", values_to = "freq") %>% 
+      group_by(`1st_network`, Age_group, Bridgeness) %>%
+      summarise_at(vars(freq), funs(geomMeanExtension(., epsilon = 1e-5))) 
+
+    list_boot[[i]] <- delta_boot_b
   }
   
-  resamples <- rbindlist(list_boot)
+  # 1000 samples for each Age_group*1st_network*Region combination
+  # So that for each <- <- <- <-  region for each RSN, and within each Age_group, 1000 resamples of the same size are taken
   
-  delta_proportion_boot_b <- resamples %>%
-    group_by(n_data_boot, n_data_split, `1st_network`, Region, Age_group) %>%
-    summarize_at(vars(Global_Bridge:Super_Bridge), mean) %>%
-    ungroup()
+  resamples <- rbindlist(list_boot) %>% as.data.frame()
   
-  # Multiplicative replacement
-  replacement_b <- delta_proportion_boot_b %>%
-    group_by(n_data_boot, .add = TRUE) %>%
-    group_split()
-  
-  list_replacement <- list()
-  for (i in 1:length(replacement_b)) {
-    list_replacement[[i]] <- multRepl(rbindlist(replacement_b[i]) %>%
-                                        dplyr::select(Global_Bridge:Super_Bridge), dl = rep(1, 4), label = 0, frac = 1e-5)
-  }
-  
-  delta_b_detail <<- cbind(
-    delta_proportion_boot_b %>%
-      dplyr::select(n_data_boot, n_data_split, `1st_network`:Age_group),
-    rbindlist(list_replacement)
-  ) %>%
-    pivot_longer(
-      cols = !c("n_data_boot", "n_data_split", "1st_network", "Region", "Age_group"),
-      names_to = "Bridgeness", values_to = "freq"
-    ) %>%
-    # Compute the difference in proportion of a given functional role within each RSN
-    group_by(n_data_boot, `1st_network`, Age_group, Bridgeness) %>%
-    summarize_at(vars(freq), funs(geometricmean(.))) %>% 
+  log_ratio_boot_b <- resamples %>% 
     group_by(n_data_boot, `1st_network`, Bridgeness) %>%
-    # Take the log ratios of geometric means
-    mutate(delta_freq = log(freq / lead(freq))) %>%
+    mutate(delta_freq = log(freq / lead(freq))) %>% 
     dplyr::select(-c(freq, Age_group)) %>%
     na.omit()
   
-  summary_bootstrap_b <<- delta_b_detail %>%
+  summary_bootstrap_b <<- log_ratio_boot_b %>%
     group_by(`1st_network`, Bridgeness) %>%
     get_summary_stats(delta_freq, type = "full") %>%
     mutate(mean_025 = mean - ci) %>%
     mutate(mean_0975 = mean + ci) %>%
     dplyr::select(`1st_network`, Bridgeness, mean, mean_025, mean_0975)
+  
+  ##############################################################################
+  ##############################################################################
+  
+  write.csv(summary_bootstrap_a, "summary_bootstrap_modular.csv")
+  write.csv(summary_bootstrap_b, "summary_bootstrap_interareal.csv")
+  
 }
 
-bootstrap_ci(n_boot = 100, "Young", "Old",
-             divergent_RSN_modular = "Auditory|CON|DMN|FPN|Language|SMN",
-             divergent_RSN_interareal = "Auditory|CON|DMN|FPN|Language|SMN")
+bootstrap_ci(n_boot = 1000, "Young", "Old",
+             RSN_modular = "Auditory|CON|DMN|FPN|Language|SMN",
+             RSN_interareal = "Auditory|CON|DMN|FPN|Language|SMN")
 
-write.csv(summary_bootstrap_a, "summary_bootstrap_modular.csv")
-write.csv(summary_bootstrap_b, "summary_bootstrap_interareal.csv")
+
 
 
 data_a <- read.csv("summary_bootstrap_modular.csv")
