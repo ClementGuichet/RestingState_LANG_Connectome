22661ad5dfbe7ee3f4b62c052dfef294991127aa
ClementGuichet <Clement.Guichet@univ-grenoble-alpes.fr>
2023-01-03 13:18:53 +0100

    Added modes of topological reconfigurations

--- _07_Entropy.R
+++ _07_Entropy.R
@@ -1,8 +1,5 @@
-rm(list = ls())
-
-source("_06_Post_clustering.R")
-
 library(philentropy)
+source("_06_Post_clustering.R")
 
 ################################################################################
 # For resting state networks
@@ -144,18 +141,20 @@ gJSD_heuristic <- function(cluster1, cluster2) {
 
   final_heuristic <<- modular_gJSD_3 %>%
     group_by(`1st_network`) %>%
-    mutate(between_cluster_var = Generalized_Jensen_Shannon_divergence / lag(Generalized_Jensen_Shannon_divergence)) %>%
+    mutate(between_cluster_var = Generalized_Jensen_Shannon_divergence / lead(Generalized_Jensen_Shannon_divergence)) %>% 
+    mutate(log = log(between_cluster_var)) %>% 
     na.omit()
 
   final_heuristic_bis <<- interareal_gJSD_3 %>%
     group_by(`1st_network`) %>%
-    mutate(between_cluster_var = Generalized_Jensen_Shannon_divergence / lag(Generalized_Jensen_Shannon_divergence)) %>%
+    mutate(between_cluster_var = Generalized_Jensen_Shannon_divergence / lead(Generalized_Jensen_Shannon_divergence)) %>% 
+    mutate(log = log(between_cluster_var)) %>% 
     na.omit()
 }
 gJSD_heuristic("Young", "Old")
 
 # Strength of between-Age_group reconfiguration
-KL_heuristic <- function(cluster1, cluster2, norm = NULL) {
+KL_heuristic <- function(cluster1, cluster2, max, min, norm = NULL) {
   data_cluster_selection(cluster1, cluster2)
   # For modular-level functional roles (i.e., Connector, Satellite ...)
   modular_KL_1 <- tmp_cluster_final %>%
@@ -263,27 +262,27 @@ KL_heuristic <- function(cluster1, cluster2, norm = NULL) {
 
   # To quantify how topological reconfiguration is taking place within each RSN between clusters
   if (norm == TRUE) {
-    Radar_RSN_modular <<- modular_KL_3 %>%
+    Radar_RSN_modular <- modular_KL_3 %>%
       merge(., final_heuristic %>% dplyr::select(`1st_network`, between_cluster_var), by = "1st_network") %>%
-      mutate(norm_KL = Kullback_Leibler_divergence / between_cluster_var) %>%
+      mutate(norm_KL = Kullback_Leibler_divergence / between_cluster_var) %>% 
       group_by(`1st_network`) %>%
-      summarize_at(vars(norm_KL), mean) %>%
+      summarize_at(vars(norm_KL), funs(geomMeanExtension(., epsilon = 1e-3))) %>%
       spread(`1st_network`, norm_KL)
 
-    Radar_RSN_interareal <<- interareal_KL_3 %>%
+    Radar_RSN_interareal <- interareal_KL_3 %>%
       merge(., final_heuristic_bis %>% dplyr::select(`1st_network`, between_cluster_var), by = "1st_network") %>%
-      mutate(norm_KL = Kullback_Leibler_divergence / between_cluster_var) %>%
+      mutate(norm_KL = Kullback_Leibler_divergence / between_cluster_var) %>% 
       group_by(`1st_network`) %>%
-      summarize_at(vars(norm_KL), mean) %>%
+      summarize_at(vars(norm_KL), funs(geomMeanExtension(., epsilon = 1e-3))) %>%
       spread(`1st_network`, norm_KL)
 
-    Radar_KL <- rbind(Radar_RSN_modular, Radar_RSN_interareal)
+    Radar_KL <<- rbind(Radar_RSN_modular, Radar_RSN_interareal)
     rownames(Radar_KL) <- c(
       "Modular-level\n PMF",
       "Interareal-level\n PMF"
     )
 
-    radarplotting_overlap(Radar_KL, 3, 0, 1, 1,
+    radarplotting_overlap(Radar_KL, max, min, 1, 1,
       alpha = 0.3, label_size = 1, title_fill = "Normalized relative entropy of the functional role probability mass functions (PMF) between the two clusters\n Computed for the PMF of each region between clusters and averaged at the RSN level",
       palette = RColorBrewer::brewer.pal(8, "Dark2")
     )
@@ -295,24 +294,24 @@ KL_heuristic <- function(cluster1, cluster2, norm = NULL) {
       text.col = "black", cex = 1, pt.cex = 2
     )
   } else {
-    Radar_RSN_modular <<- modular_KL_3 %>%
+    Radar_RSN_modular <- modular_KL_3 %>%
       group_by(`1st_network`) %>%
-      summarize_at(vars(Kullback_Leibler_divergence), mean) %>%
+      summarize_at(vars(Kullback_Leibler_divergence), funs(geomMeanExtension(., epsilon = 1e-3))) %>%
       spread(`1st_network`, Kullback_Leibler_divergence)
 
-    Radar_RSN_interareal <<- interareal_KL_3 %>%
+    Radar_RSN_interareal <- interareal_KL_3 %>%
       group_by(`1st_network`) %>%
-      summarize_at(vars(Kullback_Leibler_divergence), mean) %>%
+      summarize_at(vars(Kullback_Leibler_divergence), funs(geomMeanExtension(., epsilon = 1e-3))) %>%
       spread(`1st_network`, Kullback_Leibler_divergence)
 
-    Radar_KL <- rbind(Radar_RSN_modular, Radar_RSN_interareal)
+    Radar_KL <<- rbind(Radar_RSN_modular, Radar_RSN_interareal)
     rownames(Radar_KL) <- c(
       "Modular-level\n PMF",
       "Interareal-level\n PMF"
     )
 
-    radarplotting_overlap(Radar_KL, 2, 0, 1, 1,
-      alpha = 0.3, label_size = 1, title_fill = "Unnormalized relative entropy of the functional role probability mass functions (PMF) between the two clusters\n Computed for the PMF of each region between clusters and averaged at the RSN level",
+    radarplotting_overlap(Radar_KL, max, min, 1, 1,
+      alpha = 0.3, label_size = 1, title_fill = "Amount of topological reconfiguration per RSN between the two clusters\n Computed for the PMF of each region between clusters and averaged at the RSN level",
       palette = RColorBrewer::brewer.pal(8, "Dark2")
     )
 
@@ -324,23 +323,16 @@ KL_heuristic <- function(cluster1, cluster2, norm = NULL) {
     )
   }
 }
-
 # If normalized, then the radar plot displays the RSN with the most amount of topological reconfiguration
 # combined with high specificity: all regions within the RSN reconfigure in unison towards a specific functional role
-KL_heuristic("Young", "Old", norm = FALSE)
+KL_heuristic("Young", "Old", 3, 0, norm = FALSE)
 # Arbitrary threshold to pick the RSN to investigate the difference in proportions
-median(as.numeric(Radar_RSN_modular))
-median(as.numeric(Radar_RSN_interareal))
-
+# median(as.numeric(Radar_RSN_modular))
+# median(as.numeric(Radar_RSN_interareal))
+# 
 
 # Consistency computation ----
 # A RSN is considered to have a consistent reconfiguration if regions with the same functional role have the same most probable trajectory
-
-# Specificity necessarily entails consistency but the reciprocal is not true:
-# If all regions reconfigure towards a same functional role as a whole, this is necessarily true at for every functional role
-# Conversely, regions with a same functional role can be very consistent in their reconfiguration but not very specific, that is in unison, with the other consistent trajectories
-
-
 consistency <- function(type_func_df, cluster1, cluster2) {
   adjacency_to_2col <- function(dataframe) {
     crossdata <- lapply(rownames(data), function(x) sapply(colnames(data), function(y) list(x, y, data[x, y])))
@@ -384,7 +376,6 @@ consistency <- function(type_func_df, cluster1, cluster2) {
   }
 
 
-
   # Remove non-existent trajectories
   pairwise_prob <- rbindlist(pairwise_prob_list) %>%
     filter(Value != 0)
@@ -393,12 +384,7 @@ consistency <- function(type_func_df, cluster1, cluster2) {
     unite(., trajectory, c(colnames(.[, 3]), colnames(.[, 4])), remove = FALSE) %>%
     arrange(`1st_network`, Region, trajectory)
 
-  # Other approach with gJSD
-
-  # Compute each possible trajectories and their probability
-  # Normalize the probabilities for each RSN for each starting functional role
-  # Compare region, functional role avec gJSD
-  #
+  # Approach with gJSD
   consistency_gJSD_split <- consistency_1 %>%
     ungroup() %>%
     arrange(`1st_network`, Region, trajectory) %>%
@@ -407,7 +393,8 @@ consistency <- function(type_func_df, cluster1, cluster2) {
 
   gJSD_list <- list()
   for (i in 1:length(consistency_gJSD_split)) {
-    tmp <- rbindlist(consistency_gJSD_split[i]) %>%
+    tmp <- rbindlist(consistency_gJSD_split[i]) %>% 
+      # Normalize the probabilities for each RSN for each starting functional role
       group_by(Region) %>%
       mutate(norm_prob = Value / sum(Value)) %>%
       dplyr::select(`1st_network`, Region, trajectory, colnames(.[, 4]), norm_prob)
@@ -421,70 +408,118 @@ consistency <- function(type_func_df, cluster1, cluster2) {
       remove_rownames() %>%
       column_to_rownames("Region") %>%
       mutate_all(., ~ replace(., is.na(.), 0))
-
+    
+    # Compare regions within a specific RSN and for a specific functional role with gJSD
     gJSD <- philentropy::gJSD(tmp_ter %>% as.matrix())
     gJSD_list[[i]] <- cbind(tmp_bis %>% slice(1) %>% dplyr::select(`1st_network`, colnames(.[, 3])), gJSD)
   }
 
-  consistency_gJSD <<- rbindlist(gJSD_list) %>%
+  consistency_gJSD <- rbindlist(gJSD_list) %>%
     as.data.frame() %>%
     plyr::rename(c("gJSD" = "Generalized_Jensen_Shannon_divergence"))
-
-  Radar_RSN_consistency_gJSD <- consistency_gJSD %>%
-    group_by(.[1]) %>%
-    summarize_at(vars(everything()), mean) %>%
-    dplyr::select(-colnames(.[, 2])) %>%
-    spread(`1st_network`, Generalized_Jensen_Shannon_divergence)
-
-  radarplotting_overlap(Radar_RSN_consistency_gJSD, 1, 0, 1, 1,
-    inverse_grad = TRUE,
-    alpha = 0.5, label_size = 1,
-    title_fill = "gJSD Consistency",
-    palette = RColorBrewer::brewer.pal(8, "Dark2")
-  )
+  
+  if (colnames(type_func_df[4]) == "Hub_consensus") {
+    Radar_RSN_consistency_gJSD_modular <<- consistency_gJSD %>%
+      group_by(`1st_network`) %>%
+      summarize_at(vars(Generalized_Jensen_Shannon_divergence), funs(geomMeanExtension(., epsilon = 1e-3))) %>% 
+      spread(`1st_network`, Generalized_Jensen_Shannon_divergence)
+    
+    radarplotting_overlap(Radar_RSN_consistency_gJSD_modular, 1, 0, 1, 1,
+                          inverse_grad = TRUE,
+                          alpha = 0.5, label_size = 1,
+                          title_fill = "gJSD Consistency",
+                          palette = RColorBrewer::brewer.pal(8, "Dark2")
+    )
+  } else {
+    Radar_RSN_consistency_gJSD_interareal <<- consistency_gJSD %>%
+      group_by(`1st_network`) %>%
+      summarize_at(vars(Generalized_Jensen_Shannon_divergence), funs(geomMeanExtension(., epsilon = 1e-3))) %>% 
+      spread(`1st_network`, Generalized_Jensen_Shannon_divergence)
+    
+    radarplotting_overlap(Radar_RSN_consistency_gJSD_interareal, 1, 0, 1, 1,
+                          inverse_grad = TRUE,
+                          alpha = 0.5, label_size = 1,
+                          title_fill = "gJSD Consistency",
+                          palette = RColorBrewer::brewer.pal(8, "Dark2")
+    )
+  }
 }
-
 consistency(modular_KL_2, "Young", "Old")
+consistency(interareal_KL_2, "Young", "Old")
+
+# Clustering the modes of reconfiguration
+reconfig_mode <- list(
+  # Specificity
+  final_heuristic %>% dplyr::select(`1st_network`, between_cluster_var) %>% 
+    plyr::rename(c("between_cluster_var" = "Specificity_modular")),
+  final_heuristic_bis %>% dplyr::select(`1st_network`, between_cluster_var) %>% 
+    plyr::rename(c("between_cluster_var" = "Specificity_interareal")),
+  # Quantity
+  Radar_KL %>% t(.) %>% as.data.frame(.) %>% mutate(`1st_network` = rownames(.)) %>% 
+    plyr::rename(c("V1" = "Quantity_modular")) %>% plyr::rename(c("V2" = "Quantity_interareal")),
+  # Consistency
+  Radar_RSN_consistency_gJSD_modular %>% t(.) %>% as.data.frame(.) %>% mutate(`1st_network` = rownames(.)) %>% 
+    plyr::rename(c("V1" = "Consistency_modular")),
+  Radar_RSN_consistency_gJSD_interareal %>% t(.) %>% as.data.frame(.) %>% mutate(`1st_network` = rownames(.)) %>% 
+    plyr::rename(c("V1" = "Consistency_interareal"))
+  ) %>% 
+  reduce(full_join,
+  by = "1st_network") %>% 
+  remove_rownames() %>% column_to_rownames("1st_network") %>% 
+  scale(.) %>% as.data.frame()
 
 
-# # Count the number of similar trajectories
-# consistency_2 <- consistency_1 %>%
-#   group_by(`1st_network`, trajectory, .[,4]) %>%
-#   summarize(n = n()) %>%
-#   mutate(freq = n/sum(n))
-#
-# consistency_2_bis <- consistency_1 %>% distinct(Region, .keep_all = TRUE) %>%
-#   group_by(`1st_network`) %>%
-#   summarize(n_region = n())
-#
-# consistency_2_merge <- merge(consistency_2, consistency_2_bis, by = "1st_network") %>%
-#   mutate(freq = n/n_region)
-#
-# # Normalize this count by the mean probability that this trajectory occurs
-# consistency_3 <- consistency_1 %>%
-#   group_by(`1st_network`, trajectory, .[,4]) %>%
-#   summarize_at(vars(Value), mean) %>%
-#   # normalize the probability of trajectory occurence by each functional role
-#   group_by(`1st_network`, .[,3]) %>%
-#   mutate(norm_prob = Value/sum(Value)) %>%
-#   cbind(., freq = consistency_2_merge$freq) %>%
-#   # Frequency of the trajectory within the RSN multiplied by the probability
-#   mutate(consistency_score = freq*norm_prob) %>%
-#   # Keep only the highest consistency score for each functionak role
-#   group_by(`1st_network`, .[,3]) %>% slice_max(consistency_score) %>%
-#   distinct(consistency_score, .keep_all = TRUE)
-#
-# consistency_final <- consistency_3 %>%
-#   # Normalize for each starting functional role
-#   group_by(`1st_network`) %>%
-#   summarise_at(vars(consistency_score), mean)
-#
-# Radar_RSN_consistency <- consistency_final %>%
-#   spread(`1st_network`, consistency_score) %>%
-#   mutate_at(vars(everything()), funs(. * 10))
-#
-# radarplotting_overlap(Radar_RSN_consistency, 5, 1, 1, 1,
-#                       alpha = 0.3, label_size = 1,
-#                       title_fill = "Consistency score",
-#                       palette = RColorBrewer::brewer.pal(8, "Dark2")
+
+# fviz_nbclust(reconfig_mode, kmeans, method = "wss")
+# 
+# km <- kmeans(reconfig_mode, centers = 6, nstart = 25)
+# fviz_cluster(km, data = reconfig_mode)
+# 
+# 
+# gap_stat <- cluster::clusGap(reconfig_mode,
+#   FUN = hcut,
+#   K.max = 7,
+#   B = 1000,
+#   verbose = T
 # )
+# 
+# factoextra::fviz_gap_stat(gap_stat)
+
+plot_cluster <- cluster::agnes(reconfig_mode, method = "ward", metric = "euclidian")
+fviz_dend(plot_cluster,
+  cex = 0.8,
+  k = 6,
+  palette = "jco",
+  rect = T,
+  color_labels_by_k = TRUE,
+  main = "Ward Hierarchical clustering"
+)
+
+final_clust <- hclust(dist(reconfig_mode, method = "euclidean"), method = "ward.D2")
+groups <- cutree(final_clust, k = 6)
+
+
+reconfig_mode_post_clustering <- cbind(reconfig_mode, cluster = groups) %>% 
+  group_by(cluster) %>% summarize_at(vars(everything()), mean) %>%
+  mutate(cluster = ifelse(cluster == 1, "Auditory|PMM", 
+                          ifelse(cluster == 2, "CON|FPN|SMN", 
+                                        ifelse(cluster == 4, "DMN|Language|Visual_1", "Not_interesting")))) %>% 
+  filter(cluster != "Not_interesting") %>% 
+  remove_rownames() %>% column_to_rownames("cluster")
+
+Radar_reconfig_mode <- reconfig_mode_post_clustering %>% radarplotting_overlap(., 2, -2, 1, 1, label_size = 1, alpha = 0.2,
+                        title_fill = "Modes of reconfigurations",
+                        palette = RColorBrewer::brewer.pal(8, "Dark2"))
+legend(
+  x = "topright",
+  legend = rownames(reconfig_mode_post_clustering), horiz = TRUE,
+  bty = "n", pch = 20, col = RColorBrewer::brewer.pal(8, "Dark2"),
+  text.col = "black", cex = 1, pt.cex = 2
+)
+
+
+
+
+
+
+ 
